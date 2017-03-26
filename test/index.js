31c1700d4f768a91ac5b94f9bcd0ab4b5700fa87
yrong <yrong1997@gmail.com>
2017-03-26 16:57:59 +0800

    测试用例配合auth单点认证变更,确保所有项目npm test集成测试成功

--- test/index.js
+++ test/index.js
@@ -1,103 +1,23 @@
-const rp = require('request-promise')
-const config = require('config')
-const queryString = require('query-string')
-const _ = require('lodash')
-const assert = require('chai').assert
+require("babel-core/register")
+require("babel-polyfill")
 
-const json_option = {json: true}
-const get_options = _.assign({method: 'GET'},json_option)
-const post_options = _.assign({method: 'POST'},json_option)
+var newman = require('newman');
+var assert = require('chai').assert;
 
-const generateUri = (base_url, path, params)=> {return {uri: base_url + path + (params?('/?' + queryString.stringify(params)):'')}}
+describe("scmp-z api Integration test suite", function() {
+    this.timeout(15000)
 
-const apiGet = (base_url, path, params)=>{
-    return rp(_.assign(generateUri(base_url, path, params),get_options))
-}
-
-const apiPost = (base_url,path,body,params)=>{
-    return rp(_.assign(generateUri(base_url, path, params),post_options,{body:body}))
-}
-
-const legacy_url = config.get('config.legacy_url')
-const url = `http://localhost:${config.get('config.port')}/api`
-
-const convert_integer_2_string = (obj)=>{
-    for (var key in obj)
-    {
-        if (_.isNumber(obj[key]))
-            obj[key] = String(obj[key]);
-        else if(_.isObject(obj[key]))
-            obj[key] = convert_integer_2_string(obj[key])
-    }
-    return obj
-}
-
-const compare = (values,done)=>{
-    let result_new = values[0]
-    let result_legacy = values[1]
-    try{
-        assert.deepEqual((result_new),result_legacy)
-        done()
-    }catch(err){
-        done(err)
-    }
-}
-
-
-describe('scmp-z test cases', function() {
-    this.timeout(2000)
-    describe('/api/triggers', function() {
-        it('/group', function (done) {
-            Promise.all([apiGet(url, '/triggers/group'),apiGet(legacy_url, '/triggers/group')]).then(values=>{
-                compare(values,done)
-            })
+    it('postman test cases', function(done) {
+        newman.run({
+            collection: require('./scmp-z.postman_collection.json'),
+            environment: require('./scmp-z.postman_environment.json'),
+            reporters: 'cli'
+        }, function (err) {
+            if (err) { done(err)}
+            console.log('new api run complete!');
+            done();
         });
-        it('/active', function (done) {
-            Promise.all([apiGet(url, '/triggers/active'),apiGet(legacy_url, '/triggers/active')]).then(values=>{
-                compare(values,done)
-            })
-        });
-        it('/active/:group', function (done) {
-            Promise.all([apiGet(url, `/triggers/active/${encodeURIComponent('Storage')}`),apiGet(legacy_url, `/triggers/active/${encodeURIComponent('Storage')}`)]).then(values=>{
-                compare(values,done)
-            })
-        });
-    })
-    describe('/api/items/search', function() {
-        it('/search/Exchange Mailbox Database', function (done) {
-            let body = {
-                "appName":"Exchange Mailbox Database",
-                "hosts":["AS-3650-EXCDB-1","AS-3650-EXCDB-2","AS-VME-EXCCAS-1","BJ-X3650-EXC-1"],
-                "items":["Exchange Mailbox Database ==> Instances(*) I/O Database Reads Average Latency",
-                    "Exchange Mailbox Database ==> Instances(*) I/O Database Writes Average Latency",
-                    "Exchange Mailbox Database Log Bytes Write/sec","Exchange Mailbox Database Log Record Stalls/sec",
-                    "Exchange Mailbox Database Log Threads Waiting"],
-                "transposed":0,
-                "title": null
-            }
-            Promise.all([apiPost(url,'/items/search',body),apiGet(legacy_url, `/group/${encodeURIComponent('Exchange Mailbox Database')}`)]).then(values=>{
-                compare(values,done)
-            })
-        });
-        it('/search/Windows Servers', function (done) {
-            let body = {
-                "appName":"Windows Servers"
-            }
-            Promise.all([apiPost(url,'/items/search',body),apiGet(legacy_url, `/group/${encodeURIComponent('Windows Servers')}`)]).then(values=>{
-                compare(values,done)
-            })
-        });
-    })
-    describe('/api/items/history/:itemId/:since-:until', function() {
-        it('23766', function (done) {
-            let since = 1463808199,until = 1463809199,itemId = 23766
-            let body = {
-                since:since,
-                until:until
-            }
-            Promise.all([apiPost(url,`/items/history/${itemId}`,body),apiGet(legacy_url, `/history/${itemId}/${since}-${until}`)]).then(values=>{
-                compare(values,done)
-            })
-        });
-    })
-});
+    });
+
+
+})
