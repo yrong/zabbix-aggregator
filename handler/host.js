0e48343887c2a785c2b4761fc85c1245efe6e9be
yrong <yrong1997@gmail.com>
2017-06-23 12:56:46 +0800

    对比工具：对比zabbix和cmdb中的主机（3h)
    注:本次用例变更主要是切换用户导致的用例顺序变化，除了新增对比接口(参考用例:hosts compare with cmdb)外原接口无变更

--- handler/host.js
+++ handler/host.js
@@ -4,6 +4,9 @@ const Router = require('koa-router')
 const _ = require('lodash')
 const db = require('../lib/db')
 
+const rp = require('request-promise')
+const config = require('config')
+
 let hosts = new Router();
 hosts.get('/', async (ctx, next)=>{
     let params = _.assign({},ctx.params,ctx.query,ctx.request.body)
@@ -13,6 +16,24 @@ hosts.get('/', async (ctx, next)=>{
     }
     let [hosts] = await db.query(sql)
     ctx.body = hosts
+});
+
+hosts.get('/compare_with_cmdb', async (ctx, next)=>{
+    let params = _.assign({},ctx.params,ctx.query,ctx.request.body),results,cmdb_hosts=[],zabbix_hosts=[]
+    let cmdb_hosts_url = config.get('cmdb.base_url') + '/api/cfgItems?subcategory=PhysicalServer,VirtualServer',
+        options = {uri:cmdb_hosts_url,method:'GET',json:true}
+    results = await rp(options)
+    if(results.data&&results.data.length){
+        cmdb_hosts = _.map(results.data,(result)=>result.name)
+    }
+    let sql = `select name from hosts where status!=3 and flags=0 and host not like "%zabbix%"`
+    let [hosts] = await db.query(sql)
+    if(hosts&&hosts.length){
+        zabbix_hosts = _.map(hosts,(result)=>result.name)
+    }
+    let zabbix_missing = _.difference(cmdb_hosts,zabbix_hosts)
+    let cmdb_missing = _.difference(zabbix_hosts,cmdb_hosts)
+    ctx.body = {cmdb_hosts,zabbix_hosts,cmdb_missing,zabbix_missing}
 });
 
 module.exports = hosts
