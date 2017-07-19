1ebe9b57e70ea299272cbecc317bbbfee49f2b8b
yrong <yrong1997@gmail.com>
2017-07-19 12:06:16 +0800

    scmp-z --> items/history/ 对trends表查询有误(1h)
    https://hk.tower.im/projects/14f47cb3192149e0a703b0485da35f38/todos/c9ae73b2615649e4b08533e042277086/

--- sql/trends.js
+++ sql/trends.js
@@ -1,11 +1,12 @@
 
+const alias = require('./alias')
 const TrendsTable = {0:'trends',3:'trends_uint'}
 
 const getTrendsTableName = (type)=>TrendsTable[type]
 
 const sqlGetValuesWithinTimeRange = (type, itemid, since, until)=>{
     let tableName = getTrendsTableName(type)
-    let sql = `SELECT value,clock FROM ${tableName} h WHERE h.itemid=${itemid} and h.clock>${since} and h.clock<${until}`
+    let sql = `SELECT value_avg as ${alias.history_value_alias},clock as ${alias.history_clock_alias} FROM ${tableName} h WHERE h.itemid=${itemid} and h.clock>${since} and h.clock<${until}`
     return sql
 }
 
