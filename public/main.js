5a1fb65691f3b5bd77c71df19a94ae0bb3879c86
yrong <yrong1997@gmail.com>
2017-07-26 22:05:44 +0800

    demo页面 - 下拉列表选择map(2h)
    https://hk.tower.im/projects/14f47cb3192149e0a703b0485da35f38/todos/ab0977333251476381fb79216ff27631/

--- public/main.js
+++ public/main.js
@@ -12,56 +12,72 @@ $.ajax({
         "username":username,
         "password":password
     })
-}).done(function (res) {
+}).done(function (auth_res) {
     $.ajax({
-        method: "POST",
-        url: `/api/sysmaps?sysmapid=2&token=${res.data.token}`,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function(result){
-            var cy = cytoscape({
-                container: document.getElementById('cy'),
-                style: [
-                    {
-                        selector: 'node',
-                        style: {
-                            shape: 'hexagon',
-                            'background-color': 'red',
-                            label: 'data(label)'
-                        }
-                    },
-                    {
-                        selector: 'edge',
-                        style: {
-                            label: 'data(label)',
-                            'font-size': '4px',
-                            'font-weight': 'lighter',
-                            'font-style': 'italic'
-                        }
-                    }],
-            });
-            let elements = result.data.elements
-            _.each(elements,(element)=>{
-                cy.add({
-                        data: { id: element.selementid,label:element.host },
-                        position:{x:element.x,y:element.y}
-                    }
-                );
+        url: `/api/sysmaps?token=${auth_res.data.token}`,
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        timeout: 5000,
+    }).done(function (maps) {
+        _.each(maps.data, function(map) {
+            $('#map_select')
+                .append($("<option></option>")
+                    .attr("value",map.sysmapid)
+                    .text(map.name));
+        });
+        $('#map_select').change(function() {
+            var opt_sel = $(this).find('option:selected');
+            var map_sel = _.find(maps.data,function(map){return map.sysmapid==opt_sel.val()});
+            $('#cy').width(map_sel.width).height(map_sel.height);
+            $.ajax({
+                method: "POST",
+                url: `/api/sysmaps?sysmapid=${map_sel.sysmapid}&token=${auth_res.data.token}`,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(result){
+                    var cy = cytoscape({
+                        container: $('#cy'),
+                        style: [
+                            {
+                                selector: 'node',
+                                style: {
+                                    shape: 'hexagon',
+                                    'background-color': 'red',
+                                    label: 'data(label)'
+                                }
+                            },
+                            {
+                                selector: 'edge',
+                                style: {
+                                    label: 'data(label)',
+                                    'font-size': '4px',
+                                    'font-weight': 'lighter',
+                                    'font-style': 'italic'
+                                }
+                            }],
+                    });
+                    let elements = result.data.elements
+                    _.each(elements,(element)=>{
+                        cy.add({
+                                data: { id: element.selementid,label:element.host },
+                                position:{x:element.x,y:element.y}
+                            }
+                        );
+                    })
+                    let links = result.data.links
+                    _.each(links,(link)=>{
+                        cy.add({
+                            data:{id:link.linkid,label:link.label,source:link.selementid1,target:link.selementid2}
+                        })
+                    })
+                    var layout = cy.layout({
+                        name: 'preset',
+                    });
+                    layout.run();
+                }
             })
-            let links = result.data.links
-            _.each(links,(link)=>{
-                cy.add({
-                    data:{id:link.linkid,label:link.label,source:link.selementid1,target:link.selementid2}
-                })
-            })
-            var layout = cy.layout({
-                name: 'preset',
-            });
-            layout.run();
-        },
-        failure: function(err) {
-            console.log(err);
-        }
+        });
     })
 }).fail(function(err) {
     console.log(err);
