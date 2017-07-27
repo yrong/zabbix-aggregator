2d947bdd9f8b84010a8ded6b61a1a40e14da9904
yrong <yrong1997@gmail.com>
2017-07-28 00:02:23 +0800

    dump images from zabbix(2h)

---
+++ dump_image.js
@@ -0,0 +1,13 @@
+const _ = require('lodash')
+const db = require('./lib/db')
+const fs = require('fs')
+const path = require('path')
+let image_folder = path.resolve(__dirname,'public/images')
+if (!fs.existsSync(image_folder))
+    fs.mkdirSync(image_folder)
+db.query(`select * from images`).then((res)=>{
+    _.each(res[0],(image)=>{
+        fs.writeFileSync(__dirname + `/public/images/${image.imageid}.png`, image.image);
+    })
+    process.exit()
+})
