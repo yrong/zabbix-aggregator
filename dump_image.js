b388bca5e4d5d248ec6181fa3ae4f925501cfdb6
yrong <yrong1997@gmail.com>
2018-01-27 14:26:59 +0800

    scmpz拓扑节点图片导出路径调整(0.5h)

--- dump_image.js
+++ dump_image.js
@@ -3,8 +3,9 @@ const db = require('./lib/db')
 const fs = require('fs')
 const config = require('config')
 const mkdirp = require('mkdirp')
+const path = require('path')
 
-const image_folder = config.get('runtime_data.base_dir')+config.get('runtime_data.scmpz.image_dir')
+const image_folder = path.join((process.env['RUNTIME_PATH']||'../runtime')+config.get('runtime_data.scmpz.image_dir'))
 mkdirp.sync(image_folder)
 db.query(`select * from images`).then((res)=>{
     _.each(res,(image)=>{
