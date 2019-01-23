4847a4f5de85e98b54e533b680a602611ff4d85b
yrong <yrong1997@gmail.com>
2019-01-23 17:24:52 +0800

    监控配合组件升级＆监控节点图标配合配置项调整＆用例测试(1.5h)

--- dump_image.js
+++ dump_image.js
@@ -1,11 +1,10 @@
 const _ = require('lodash')
 const db = require('./lib/db')
 const fs = require('fs')
-const config = require('config')
 const mkdirp = require('mkdirp')
 const path = require('path')
 
-const image_folder = path.join((process.env['RUNTIME_PATH']||'../runtime')+config.get('runtime_data.scmpz.image_dir'))
+const image_folder = path.join((process.env['RUNTIME_PATH']||'../runtime')+'/scmpz/images')
 mkdirp.sync(image_folder)
 db.query(`select * from images`).then((res)=>{
     _.each(res,(image)=>{
